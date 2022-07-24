176e1a12456bc197f47591d118e46320be254b66
MaxMcRolly <m.prylutski@gmail.com>
2022-07-24 17:43:51 +0300

    Added some local imports

--- lib/imports.dart
+++ lib/imports.dart
@@ -1,3 +1,6 @@
 export "screens/login.dart";
 export 'package:flutter/material.dart';
 export 'package:bonkbonk/models/user.dart';
+
+export 'package:bonkbonk/logic/page_view/page_view_cubit.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
