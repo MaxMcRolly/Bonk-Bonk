b50d45f99010c8679269a84802d825b29791fa94
MaxMcRolly <m.prylutski@gmail.com>
2022-07-24 17:43:41 +0300

    Added user login query

--- lib/http_repository.dart
+++ lib/http_repository.dart
@@ -1,12 +1,31 @@
+import 'dart:convert';
+
 import "imports.dart";
 import "package:http/http.dart" as http;
 
 class HttpRepository {
   HttpRepository();
   registerUser() async {
-    String url = "http://bonk-bonk.herokuapp.com/api/auth/sign-up/";
+    String url = "https://bonk-bonk.herokuapp.com/api/auth/sign-up/";
     var response = await http.post(Uri.parse(url), body: user.toJson());
     print(response.body);
     print(response.statusCode);
+  }
+
+  Future<bool> loginUser(
+      {required String login, required String password}) async {
+    String url = "https://bonk-bonk.herokuapp.com/api/auth/login/";
+    var response = await http.post(Uri.parse(url), headers: {
+      "Authorization": "Basic ${base64.encode(utf8.encode("$login:$password"))}"
+    });
+    print(response.body);
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      user = User.fromJson(json.decode(response.body)["user"]);
+      user.token = json.decode(response.body)["token"];
+      return true;
+    } else {
+      return false;
+    }
   }
 }
