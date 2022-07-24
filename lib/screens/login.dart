aa99481bf1c2109f9c65c47ca9a0564e08adfb43
MaxMcRolly <m.prylutski@gmail.com>
2022-07-24 17:45:17 +0300

    Added logic for succesful logic case

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,9 @@
+import 'package:bonkbonk/http_repository.dart';
 import "package:bonkbonk/imports.dart";
+import 'package:bonkbonk/screens/home.dart';
 import 'package:bonkbonk/screens/register.dart';
 import 'package:bonkbonk/widgets/logo.dart';
+import 'package:flutter/cupertino.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,11 +13,18 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _loginController = TextEditingController();
+
+  TextEditingController _passwordController = TextEditingController();
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    _loginController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController _loginController = TextEditingController();
-
-    TextEditingController _passwordController = TextEditingController();
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(title: Text("bonk-bonk")),
@@ -51,7 +61,32 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 12,
             ),
             OutlinedButton(
-              onPressed: () {},
+              onPressed: () async {
+                await HttpRepository().loginUser(
+                  login: _loginController.text,
+                  password: _passwordController.text,
+                )
+                    ? Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()))
+                    : ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Неверные логин/пароль")));
+                // : showDialog(
+                //     context: context,
+                //     builder: ((context) => AlertDialog(
+                //           title: Text('Неверные логин/пароль',
+                //               textAlign: TextAlign.center,
+                //               style: TextStyle(fontSize: 16)),
+                //           actions: [
+                //             Center(
+                //               child: ElevatedButton(
+                //                   onPressed: () {
+                //                     Navigator.pop(context);
+                //                   },
+                //                   child: Text("OK :(")),
+                //             )
+                //           ],
+                //         )));
+              },
               child: Text("Войти"),
             ),
             TextButton(
