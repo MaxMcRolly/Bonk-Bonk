01df85b9735b50904ab77de28f82b649a6783e1f
MaxMcRolly <m.prylutski@gmail.com>
2022-07-25 11:27:40 +0300

    Added get user data query

--- lib/http_repository.dart
+++ lib/http_repository.dart
@@ -1,19 +1,27 @@
 import 'dart:convert';
 
+import 'package:bonkbonk/main.dart';
+
 import "imports.dart";
 import "package:http/http.dart" as http;
 
 class HttpRepository {
   HttpRepository();
-  registerUser() async {
+  Future registerUser() async {
     String url = "https://bonk-bonk.herokuapp.com/api/auth/sign-up/";
     var response = await http.post(Uri.parse(url), body: user.toJson());
     print(response.body);
     print(response.statusCode);
+    // if (response.statusCode == 201) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    return response;
   }
 
-  Future<bool> loginUser(
-      {required String login, required String password}) async {
+  // Future<bool> loginUser(
+  Future loginUser({required String login, required String password}) async {
     String url = "https://bonk-bonk.herokuapp.com/api/auth/login/";
     var response = await http.post(Uri.parse(url), headers: {
       "Authorization": "Basic ${base64.encode(utf8.encode("$login:$password"))}"
@@ -22,7 +30,31 @@ class HttpRepository {
     print(response.statusCode);
     if (response.statusCode == 200) {
       user = User.fromJson(json.decode(response.body)["user"]);
-      user.token = json.decode(response.body)["token"];
+      authToken = json.decode(response.body)["token"];
+      localStorage.setString("token", authToken!);
+      // return true;
+    } else {
+      // return false;
+    }
+    return response;
+  }
+
+  getUserData() async {
+    String url = "https://bonk-bonk.herokuapp.com/api/user/";
+    var response = await http
+        .get(Uri.parse(url), headers: {"Authorization": "Token ${authToken}"});
+    if (response.statusCode == 200) {
+      user = User.fromJson(json.decode(response.body));
+    }
+    print(response.body);
+    print(response.statusCode);
+  }
+
+  Future<bool> checkLoginAvailability({required String username}) async {
+    String url = "https://bonk-bonk.herokuapp.com/api/auth/check-username/";
+    var response =
+        await http.post(Uri.parse(url), body: {"username": username});
+    if (response.statusCode == 200) {
       return true;
     } else {
       return false;
