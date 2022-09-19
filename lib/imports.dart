6788e57a3cf10c037963cd9ad5e02ea758aca310
mprilutski <mprilutski@mapsoft.by>
2022-09-19 17:04:57 +0300

    Added imports

--- lib/imports.dart
+++ lib/imports.dart
@@ -1,6 +1,8 @@
 export "screens/login.dart";
 export 'package:flutter/material.dart';
 export 'package:bonkbonk/models/user.dart';
+export 'package:bonkbonk/http_repository.dart';
+export 'package:bonkbonk/models/player.dart';
 
 export 'package:bonkbonk/logic/page_view/page_view_cubit.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
